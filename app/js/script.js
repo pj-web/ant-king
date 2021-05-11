40550cbb0d4b45636325ff7f27ef3463c5d2e28a
pj-web <insta.pj.05@gmail.com>
2021-05-12 01:46:52 +0300

    main page almost ready 210512

--- app/js/script.js
+++ app/js/script.js
@@ -43,90 +43,29 @@ if (iconMenu) {
     });
 }
 
-function getDayEnd() {
-    var dayEnd = new Date();
-    dayEnd.setHours(23, 59, 59);
 
-    return dayEnd;
-}
+// let date = new Date('May 11 2021 17:20:00');
 
-if (window.dayEndTimestamp === undefined) {
-    window.dayEndTimestamp = getDayEnd();
-}
+// function counts() {
+//     let now = new Date();
+//     gap = date - now;
 
-function getTimeRemaining(endtime) {
-    var t       = Date.parse(endtime) - Date.parse(new Date()),
-        seconds = Math.floor((t / 1000) % 60),
-        minutes = Math.floor((t / 1000 / 60) % 60),
-        hours   = Math.floor((t / (1000 * 60 * 60)) % 24);
+//     let days = Math.floor(gap / 1000 / 60 / 60 / 24);
+//     let hours = Math.floor(gap / 1000 / 60 / 60 ) % 24;
+//     let minutes = Math.floor(gap / 1000 / 60 ) % 60;
+//     let seconds = Math.floor(gap / 1000 ) % 60;
 
-    return {
-        'total':   t,
-        'hours':   hours,
-        'minutes': minutes,
-        'seconds': seconds
-    };
-}
+//     if (gap < 0) {
+//         days = days + 1;
+//         hours = hours + 24;
+//         minutes = minutes + 60;
+//         seconds = seconds + 60;
+//     }
+//     document.getElementById('days').innerText = days;
+//     document.getElementById('hours').innerText = hours;
+//     document.getElementById('minutes').innerText = minutes;
+//     document.getElementById('seconds').innerText = seconds;
+// }
+// counts();
 
-function getCookie(name) {
-    var matches = document.cookie.match(new RegExp(
-        '(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)'
-    ));
-
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-}
-
-function initializeClock(id, endtime) {
-    var clock       = document.getElementById(id),
-        hoursSpan   = clock.querySelector('.hours'),
-        minutesSpan = clock.querySelector('.minutes'),
-        secondsSpan = clock.querySelector('.seconds');
-
-    function updateClock() {
-        var t = getTimeRemaining(endtime);
-
-        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-        if (t.total <= 0) {
-            hoursSpan.innerHTML = ('00');
-            minutesSpan.innerHTML = ('00');
-            secondsSpan.innerHTML = ('00');
-            clearInterval(timeinterval);
-        }
-    }
-
-    updateClock();
-
-    var timeinterval = setInterval(updateClock, 500);
-}
-
-var myClock = (document.cookie && document.cookie.match('myClock')) ?
-                (Date.parse(getCookie('myClock')) - Date.now() > -60000) : null;
-
-// if there's a cookie with the name myClock, and timer stoped less then 1 minute (-60000) ago, use that value as the deadline
-if (myClock) {
-    // get deadline value from cookie
-    var deadline = getCookie('myClock');
-}
-// otherwise, set a deadline 45 minutes from now and
-// save it in a cookie with that name
-else {
-    // create deadline 45 minutes from now
-    var timeInMinutes = 45,
-        currentTime   = Date.parse(new Date().toString()),
-        deadline      = new Date(currentTime + timeInMinutes * 60 * 1000);
-
-    // store deadline in cookie for future reference
-    document.cookie = 'myClock=' + deadline;
-}
-
-$(document).ready(function() {
-    initializeClock('timer', getDayEnd());
-    initializeClock('timer2', getDayEnd());
-    initializeClock('timer3', getDayEnd());
-    initializeClock('timer4', getDayEnd());
-    initializeClock('timer5', getDayEnd());
-});
-
+// setInterval(counts, 1000);
