9f4f67621a745b585f6d940421246cea8bb9d1c8
pj-web <insta.pj.05@gmail.com>
2021-05-15 01:33:29 +0300

    animation is half done

--- app/js/script.js
+++ app/js/script.js
@@ -169,6 +169,10 @@ document.addEventListener('keydown', function (e) {
     }
 });
 
+
+
+
+
 //<![CDATA[
     var remain_bv   = 80768;
     function parseTime_bv(timestamp){
@@ -204,5 +208,44 @@ document.addEventListener('keydown', function (e) {
         }, 1000);
     });
     //]]>
+
+    let animItems = document.querySelectorAll('.anim-items');
+
+    if (animItems.length > 0) {
+        window.addEventListener('scroll', animOnScroll);
+        function animOnScroll() {
+            for (let index = 0; index < animItems.length; index++) {
+                const animItem = animItems[index];
+                const animItemHeight = animItem.offsetHeight;
+                const animItemOffset = offset(animItem).top;
+                const animStart = 4;
+
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+                if (animItemHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight / animStart;
+                }
+
+                if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                    animItem.classList.add('active');                    
+                } else {
+                    if (!animItem.classList.contains('anim-hide')) {
+                        animItem.classList.remove('.active');
+                    }
+                }
+            }
+        }
+        function offset(el) {
+            const rect = el.getBoundingClientRect(),
+                scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+                scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+        }
+        setTimeout(() => {
+            animOnScroll();
+        }, 300);
+    }
+
+    
 
 
